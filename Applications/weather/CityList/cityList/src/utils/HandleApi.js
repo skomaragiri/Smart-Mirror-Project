e0422f33b9f8531c3f947e2ef644092502bd5348
skomaragiri <71363974+skomaragiri@users.noreply.github.com>
2023-05-09 13:43:13 -0700

    Add files via upload
    City List commit

---
+++ Applications/weather/CityList/cityList/src/utils/HandleApi.js
@@ -0,0 +1,53 @@
+import axios from 'axios'
+
+const baseUrl = "http://localhost:27017"
+
+const getAllToDo = (setToDo) => {
+    axios
+        .get(`http://localhost:27017/`)
+        .then(({ data }) => {
+            console.log('data ---> ', data);
+            setToDo(data)
+        })
+}
+
+const addToDo = (text, setText, setToDo) => {
+
+    axios
+        .post(`http://localhost:27017/save`, { text })
+        .then((data) => {
+            console.log(data);
+            setText("")
+            getAllToDo(setToDo)
+        })
+        .catch((err) => console.log(err))
+
+}
+
+const updateToDo = (toDoId, text, setToDo, setText, setIsUpdating) => {
+
+    axios
+        .post(`http://localhost:27017/update`, { _id: toDoId, text })
+        .then((data) => {
+            setText("")
+            setIsUpdating(false)
+            getAllToDo(setToDo)
+        })
+        .catch((err) => console.log(err))
+
+}
+
+const deleteToDo = (_id, setToDo) => {
+
+    axios
+        .post(`http://localhost:27017/delete`, { _id })
+        .then((data) => {
+            console.log(data)
+            getAllToDo(setToDo)
+        })
+        .catch((err) => console.log(err))
+
+}
+
+
+export { getAllToDo, addToDo, updateToDo, deleteToDo }
