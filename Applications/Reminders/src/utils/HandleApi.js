a0d4bf1c8f90dbb57f9263a307bec1d4b4fbdecf
skomaragiri <71363974+skomaragiri@users.noreply.github.com>
2023-05-09 13:47:10 -0700

    Add files via upload
    Reminders app commit

---
+++ Applications/Reminders/src/utils/HandleApi.js
@@ -0,0 +1,53 @@
+import axios from 'axios'
+
+const baseUrl = "http://localhost:5000"
+
+const getAllToDo = (setToDo) => {
+    axios
+        .get(`http://localhost:5000/`)
+        .then(({ data }) => {
+            console.log('data ---> ', data);
+            setToDo(data)
+        })
+}
+
+const addToDo = (text, setText, setToDo) => {
+
+    axios
+        .post(`http://localhost:5000/save`, { text })
+        .then((data) => {
+            console.log(data);
+            setText("")
+            getAllToDo(setToDo)
+        })
+        .catch((err) => console.log(err))
+
+}
+
+const updateToDo = (toDoId, text, setToDo, setText, setIsUpdating) => {
+
+    axios
+        .post(`http://localhost:5000/update`, { _id: toDoId, text })
+        .then((data) => {
+            setText("")
+            setIsUpdating(false)
+            getAllToDo(setToDo)
+        })
+        .catch((err) => console.log(err))
+
+}
+
+const deleteToDo = (_id, setToDo) => {
+
+    axios
+        .post(`http://localhost:5000/delete`, { _id })
+        .then((data) => {
+            console.log(data)
+            getAllToDo(setToDo)
+        })
+        .catch((err) => console.log(err))
+
+}
+
+
+export { getAllToDo, addToDo, updateToDo, deleteToDo }
